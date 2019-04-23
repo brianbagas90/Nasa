4b012281fc230f739bd0ee38e2c8a2d5d383881f
brianbagas90 <48430981+brianbagas90@users.noreply.github.com>
2019-04-23 15:40:07 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=4fo0hzcdmr2ulrL1INX9qF5Ma58mprbHuvOIapeM";
         
         $("#loadImage").click(function(){
             $.ajax({
